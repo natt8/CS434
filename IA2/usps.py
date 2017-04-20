fixes overflow issue

IA2/usps.py
@@ -17,41 +17,33 @@ def column(dataset, i):
 
 def batch_gradient_descent(data, y, w):
 	convergence = False
-	learningRate = .000001
+	learningRate = .0000001
 	errors = 0
 	yHat = 0
 	x = 0
+	max_exponent = 0;
 	errorList = []	
 	while (convergence != True):
 		d = np.zeros((len(data[0]),1))
 		dataT = data.T
 		print "----------------------------------------------"
-		for i in range(1, 1400):
-			print "i ", i
-			print "w shape"
-			print (w).shape
-			print "y"
-			print y.shape
-			print data[i-1].shape
-			yHat = 1 / (1 + math.exp(((-1 *(w)) * data[i-1].T)))
-			print yHat	
-			errors = y[i] - yHat
-			print "errors"
+		for i in range(1, len(data)):
+			exponent = (-1*(w)) * (data[i-1].T)
+			yHat = 1/(1+ math.exp(exponent))
 
-			#print errors[0]
-			#print errors[0].shape
+			errors = y[i] - yHat
+			#print "errors"
+
 			errorList.append(errors)		
 			d = d + (errors.item(0) * data[i])
-			print "d shape"
-			print d.shape
+
 		w = w + (learningRate * d) 
+
 		if(x < 10):#w < error):
 			#convergence = True;	
 			x = x + 1
 		else:
 			convergence = True		
-		#print "d"
-		#print d
 	return d
 
 def get_train_data():
@@ -79,31 +71,7 @@ def get_train_data():
 dataNo, data, y = get_train_data()
 w = matrix(np.zeros(256))
 
-#print "w"
-#print w
-#print "len y"
-#print len(y)
-
-#print y
-
-#print "len data"
-#print len(data)
-#print "len data 0"
-#print len(data[0])
-
-#print "len w"
-#print len(w)
-
-#print "data 0"
-#print data[0]
-
-#print y.shape
-#print w.shape
-#print data.shape
-for i in range(0, len(data), 10): 
-	dataBatch = data[[i, i+9],]
-	yBatch = y[[i, i+9],]
-	d = batch_gradient_descent(data, y, w)	
-	print d
+d = batch_gradient_descent(data, y, w)	
+print d
 print w
 print d